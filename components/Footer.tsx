36530dc3787ce1648369ee30dbfdb6931434816d
gvmoraes79 <gvmoraes@gmail.com>
2025-11-25 20:05:41 -0300

    Delete components/Footer.tsx

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer: React.FC = () => (
-    <footer className="bg-slate-800 border-t border-slate-700 py-4">
-        <div className="container mx-auto text-center text-slate-500 text-sm">
-            <p>Powered by Google Gemini.</p>
-        </div>
-    </footer>
-);
